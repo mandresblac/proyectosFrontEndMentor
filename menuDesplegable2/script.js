6e214077621f6735dc03aae022ed2c28e3b30dc8
manuel blanco <mandresblac@gmail.com>
2023-11-22 14:16:19 -0500

    commit 13

--- menuDesplegable2/script.js
+++ menuDesplegable2/script.js
@@ -1,46 +1,32 @@
 const btnMenu = document.querySelector("#header__btn");
 const nav = document.querySelector("#header__nav");
-const links = document.querySelectorAll("#header__link");
-const subMenus = document.querySelectorAll("#header__submenu");
-// console.log(links);
-console.log(subMenus);
+const tagDetails = document.querySelectorAll("details");
+console.log(tagDetails);
 
-// Muestra el Menu principal
 btnMenu.addEventListener("click", () => {
+  // Muestra el Menu principal
   btnMenu.classList.toggle("mostrar-menu");
   nav.classList.toggle("mostrar-menu");
+
+  /*  Logica para cuando se haga click en el boton de cerrar, tambien cierren los submenus.
+  1- Una vez obtenidas todas las etiquetas "details" del documento html (en la línea 3 de este docuemnto) obtenemos un NodeList de 3 elementos que es como un array el cual podemos iterar con un forEach */
+  tagDetails.forEach((tag) => {
+    /* 3 - Verificamos si el elemento tag contiene el atributo "open" (con tag.hasAttribute("open")) y si lo tiene le decimos que se lo quite (con tag.removeAttribute("open")), de esta manera los submenus se cerraran automaticamente al cerrar el menu principal */
+    if (tag.hasAttribute("open")) {
+      tag.removeAttribute("open");
+    }
+  });
 });
 
-// Muestra y oculta los submenus
-// Submenu 1
-links[0].addEventListener("mouseover", () => {
-  subMenus[0].classList.add("mostrar-submenu");
-  subMenus[1].classList.remove("mostrar-submenu");
-  subMenus[2].classList.remove("mostrar-submenu");
-});
-links[0].addEventListener("mouseout", () => {
-  subMenus[1].classList.remove("mostrar-submenu");
-  subMenus[2].classList.remove("mostrar-submenu");
-});
-
-// Submenu 2
-links[1].addEventListener("mouseover", () => {
-  subMenus[1].classList.add("mostrar-submenu");
-  subMenus[0].classList.remove("mostrar-submenu");
-  subMenus[2].classList.remove("mostrar-submenu");
-});
-links[1].addEventListener("mouseout", () => {
-  subMenus[0].classList.remove("mostrar-submenu");
-  subMenus[2].classList.remove("mostrar-submenu");
-});
-
-// Submenu 3
-links[2].addEventListener("mouseover", () => {
-  subMenus[2].classList.add("mostrar-submenu");
-  subMenus[0].classList.remove("mostrar-submenu");
-  subMenus[1].classList.remove("mostrar-submenu");
-});
-links[2].addEventListener("mouseout", () => {
-  subMenus[0].classList.remove("mostrar-submenu");
-  subMenus[1].classList.remove("mostrar-submenu");
+/* Loagica para que salo una etiqueta del subMenu este abierta a la vez */
+tagDetails.forEach((tag) => {
+  // Agrega un evento de clic a cada tag
+  tag.addEventListener("click", () => {
+    // Cierra todas las etiquetas details excepto la actual
+    tagDetails.forEach((otherTag) => {
+      if (otherTag !== tag) {
+        otherTag.removeAttribute("open");
+      }
+    });
+  });
 });
