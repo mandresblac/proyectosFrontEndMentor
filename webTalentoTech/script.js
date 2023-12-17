704be1b77033641366f3ec21c8915e165d2ac588
manuel blanco <mandresblac@gmail.com>
2023-12-16 20:35:50 -0500

    commit 24

--- webTalentoTech/script.js
+++ webTalentoTech/script.js
@@ -1,1 +1,15 @@
+const btnOpenMenu = document.querySelector("#header__openMenu");
+const btnCloseMenu = document.querySelector("#header__closeMenu");
+const menu = document.querySelector("#header__nav");
 
+btnOpenMenu.addEventListener("click", () => {
+  menu.classList.add("mostrar-menu");
+  btnOpenMenu.style.display = "none";
+  btnCloseMenu.style.display = "block";
+});
+
+btnCloseMenu.addEventListener("click", () => {
+  menu.classList.remove("mostrar-menu");
+  btnOpenMenu.style.display = "block";
+  btnCloseMenu.style.display = "none";
+});
