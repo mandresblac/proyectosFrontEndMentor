820a21b3e4f1d6da262e00ff42d0659ac6c90788
manuel blanco <mandresblac@gmail.com>
2023-12-18 22:06:13 -0500

    commit 27

--- webTalentoTech/script.js
+++ webTalentoTech/script.js
@@ -1,7 +1,11 @@
 const btnOpenMenu = document.querySelector("#header__openMenu");
 const btnCloseMenu = document.querySelector("#header__closeMenu");
 const menu = document.querySelector("#header__nav");
+const dialogBox = document.querySelector("#footer__dialogBox");
+const btnOpenDialog = document.querySelector("#footer__btnOpenDialog");
+const btnCloseDialog = document.querySelector("#footer__btnCloseDialog");
 
+/* Botones para abrir y cerrar el menu principal */
 btnOpenMenu.addEventListener("click", () => {
   menu.classList.add("mostrar-menu");
   btnOpenMenu.classList.toggle("ocultar");
@@ -12,4 +16,13 @@ btnCloseMenu.addEventListener("click", () => {
   menu.classList.remove("mostrar-menu");
   btnCloseMenu.classList.toggle("mostrar");
   btnOpenMenu.classList.toggle("ocultar");
+});
+
+/* Botones para abrir y cerrar la ventana modal del Footer */
+btnOpenDialog.addEventListener("click", () => {
+  dialogBox.showModal();
+});
+
+btnCloseDialog.addEventListener("click", () => {
+  dialogBox.close();
 });
