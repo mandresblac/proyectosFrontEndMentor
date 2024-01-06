e00deabee030ec6bac8e728f98d37a3fe22e0c20
manuel blanco <mandresblac@gmail.com>
2024-01-05 19:31:51 -0500

    commit 36

--- webMasEmpleo/app.js
+++ webMasEmpleo/app.js
@@ -1,1 +1,7 @@
+const btnOpenMenu = document.querySelector("#header__openMenu");
+const btnCloseMenu = document.querySelector("#header__closeMenu");
+const nav = document.querySelector("#header__nav");
 
+btnCloseMenu.addEventListener("click", () => {
+  nav;
+});
